276c413544ccdad0f5ac84c051a2eea24ad3a634
Telmo Sampaio <telmoasampaio@gmail.com>
2022-12-11 14:03:17 +0000

    book class constructor

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Book
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+namespace Grade_Book
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Book book2 = new Book();
+            //book2.AddGrade(90.1);
+
+            var book = new Book();
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            //var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            //var result = numbers[0];
+            //result += numbers[1];
+            //result += numbers[2];
+
+            var result = 0.0;
+
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N3}");
+        }
+    }
+}
